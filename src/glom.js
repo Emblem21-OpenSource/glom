a4e8a11bc6289930f85a3ab4b48b6c4e8407d851
Patrick Ryan <12dcode@gmail.com>
2017-06-02 22:48:38 -0700

    Fixed common use cases for glom

--- src/glom.js
+++ src/glom.js
@@ -13,11 +13,14 @@ var parameterCache = {};
  * @param  {[type]} action     [description]
  * @return {[type]}            [description]
  */
-function populateParameters(action, glom) {
-  var actionName = action.prototype.constructor.name;
-  if (!parameterCache[actionName]) {
+function populateParameters(signature, chainIndex, action, glom) {
+  if(parameterCache[signature] === undefined) {
+    parameterCache[signature] = {};
+  }
+
+  if (parameterCache[signature][chainIndex] === undefined) {
     // Analyze the function string and extract the parameter names (Works with ES6 as well)
-    parameterCache[actionName] = [];
+    parameterCache[signature][chainIndex] = [];
 
     var fnStr = action.toString().replace(COMMENTS_REGEX, '');
     var argsList = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')'));
@@ -27,7 +30,7 @@ function populateParameters(action, glom) {
       for (var i = 0; i < validArgs.length - 1; i++) {
         parameter = validArgs[i].replace(SPACE_AND_COMMAS_REGEX, '');
         if(parameter !== 'next' && parameter !== 'error') {
-          parameterCache[actionName].push(parameter);
+          parameterCache[signature][chainIndex].push(parameter);
         }
       }
     }
@@ -35,9 +38,9 @@ function populateParameters(action, glom) {
 
   // Populate the result based on the intersection of the parameters and the glom
   var result = [];
-  var len = parameterCache[actionName].length;
+  var len = parameterCache[signature][chainIndex].length;
   for (i = 0; i < len; i++) {
-    result.push(glom[parameterCache[actionName][i]]);
+    result.push(glom[parameterCache[signature][chainIndex][i]]);
   }
 
   return result;
@@ -71,14 +74,16 @@ function defaultError(e) {
  * @param {Function} error  [description]
  */
 function Glom(glom, chain, options) {
-  var actions;
-  var errorMessages;
-  var errorHandlers;
   var self = this;
+  var signature = '';
+
+  for(var i = 0, len = chain.length; i < len; i++) {
+    signature += chain[i].toString();
+  }
 
   if (options === undefined) {
     options = {
-      debug: true
+      debug: false
     };
   }
 
@@ -94,7 +99,6 @@ function Glom(glom, chain, options) {
     }
   };
 
-
   /**
    * Assembles an action
    * @param  {Function} action   [description]es
@@ -103,55 +107,48 @@ function Glom(glom, chain, options) {
    * @return {Function}          [description]
    */
   function assembleAction(action, chainIndex, errors) {
-
+    var actionName = action.prototype.constructor.name;
     /**
      * Handles an action
      * @param  {Object}   glom [description]
      * @param  {Function} next [description]
      */
     return function actionHandler(glom, next) {
-      self.log('ActionHandler', 'Start');
-      try {
-        var parameters = populateParameters(action, glom);
-        self.log('ActionHandler::parameters', parameters);
-        parameters.push(function actionDone(newGlom) {
-          self.log('ActionDone', 'Action complete');
-          if ((!!newGlom) && (newGlom.constructor === Object)) {
-            // newGlom is an object literal
-            self.log('ActionDone::fusingStart', newGlom);
-            // Apply new glom changes to the glom object
-            for (var i in newGlom) {
-              if (newGlom.hasOwnProperty(i)) {
-                glom[i] = newGlom[i];
-              }
+      var parameters = populateParameters(signature, chainIndex, action, glom);
+      self.log(actionName + '::parameters', parameters);
+      parameters.push(function actionDone(newGlom) {
+        self.log(actionName, 'Complete');
+        if ((!!newGlom) && (newGlom.constructor === Object)) {
+          // newGlom is an object literal
+          self.log(actionName + '::fusingStart', newGlom);
+          // Apply new glom changes to the glom object
+          for (var i in newGlom) {
+            if (newGlom.hasOwnProperty(i)) {
+              glom[i] = newGlom[i];
             }
-            self.log('ActionDone::fusingComplete', glom);
-            // Move to the next action
-            self.log('ActionDone', 'Complete');
-            next(null, glom);
-          } else {
-            // No glom was returned
-            self.log('ActionDone', 'Complete');
-            next(null, glom);
           }
-        });
-        parameters.push(function actionWarning(errorMessage) {
-          // An non-critical warning has occured
-          errors.push(errorMessage);
-          next(glom);
-        });
-
-        self.log('ActionHandler', 'Apply');
-        action.apply(self, parameters);
-      } catch (e) {
-        // A critical error has occured
-        self.log('ActionHandler::Error', e);
-        errors.push(e);
-        next(null, {
+          self.log(actionName + '::fusingComplete', glom);
+          // Move to the next action
+          self.log(actionName, 'Exit');
+          next(null, glom);
+        } else {
+          // No glom was returned
+          self.log(actionName, 'Exit');
+          next(null, glom);
+        }
+      });
+      parameters.push(function actionError(errorMessage) {
+        // An non-critical warning has occured
+        self.log(actionName + '::error', errorMessage);
+        errors.push(errorMessage);
+        next({
           chainIndex: chainIndex,
           messages: errors
         });
-      }
+      });
+
+      self.log(actionName, 'Call');
+      action.apply(self, parameters);
     };
   }
 
@@ -161,10 +158,10 @@ function Glom(glom, chain, options) {
    * @return {[type]}        [description]
    */
   this.run = function run(done, error) {
-    this.log('Run', 'Assembling...');
-    actions = [createInitialAction(glom)];
-    errorMessages = [];
-    errorHandlers = [];
+    this.log('Glom', 'Assembling...');
+    var actions = [createInitialAction(glom)];
+    var errorMessages = [];
+    var errorHandlers = [];
     this.snapshot = new Snapshot();
     var self = this;
 
@@ -175,12 +172,12 @@ function Glom(glom, chain, options) {
     for (var i = 0, len = chain.length; i < len; i++) {
       (function lexicalWrapper(index) {
         if (chain[index] instanceof Array) {
-          self.log('lexicalWrapper', index + '.) Assembling Glom...');
+          self.log('Glom', index + '.) Assembling Glom...');
           // Assemble the action with a Glom error chain
           actions.push(assembleAction(chain[index].slice(0, 1), index, errorMessages));
           errorHandlers.push(new Glom(glom, chain[index].slice(1), error, error));
         } else {
-          self.log('lexicalWrapper', index + '.) Assembling Action: ' + chain[index].prototype.constructor.name);
+          self.log('Glom', index + '.) Assembling Action: ' + chain[index].prototype.constructor.name);
           // Assemble a normal action
           actions.push(assembleAction(chain[index], index, errorMessages));
           errorHandlers.push(error);
@@ -188,21 +185,21 @@ function Glom(glom, chain, options) {
       }(i));
     }
 
-    this.log('Run', 'Start');
+    this.log('Glom', 'Start');
     async.waterfall(actions, function glomComplete(errorObject) {
-      self.log('Run', 'Complete');
+      self.log('Glom', 'Complete');
       if (errorObject) {
-        self.log('Run::Error', errorObject);
+        self.log('Glom::Error', errorObject);
         var errorHandler = errorHandlers[errorObject.chainIndex];
         if (errorHandler instanceof Glom) {
-          self.log('Run', 'Error Callback as Glom');
+          self.log('Glom', 'Error Callback as Glom');
           errorHandler.run(error, error);
         } else {
-          self.log('Run', 'Error Callback');
-          errorHandler(errorObject.messages);
+          self.log('Glom', 'Error Callback');
+          errorHandler.call(this, errorMessages, glom);
         }
       } else {
-        self.log('Run', 'Exit');
+        self.log('Glom', 'Exit');
         done(glom);
       }
     });
--- tests/GlomTest.js
+++ tests/GlomTest.js
@@ -1,59 +1,95 @@
 var Glom = require('../src/glom.js');
 var assert = require('assert');
 
+function passthru(next) {
+  next();
+}
+
+function add(bob, next) {
+  next({
+    bob: bob + 4
+  });
+}
+
+function failCheck(bob, next, error) {
+  if (bob === 1) {
+    next();
+  } else {
+    error('This failed');
+  }
+}
+
+function fail(next, error) {
+  error('This failed');
+}
+
+function error() {
+  assert.fail('This should not have been called');
+}
+
 // Passthru test
 new Glom({
   bob: 5
 }, [
-  function passthru(bob, next) {
-    next();
-  }
+  passthru
 ]).run(function done(glom) {
   assert.equal(glom.bob, 5);
-});
+}, error);
 
 // Simple mutation test
 new Glom({
   bob: 5
 }, [
-  function add(bob, next) {
-    next({
-      bob: bob + 4
-    });
-  }
+  add
 ]).run(function done(glom) {
   assert.equal(glom.bob, 9);
-});
+}, error);
 
 // Multi-step mutation
 new Glom({
   bob: 5
 }, [
-  function add(bob, next) {
-    next({
-      bob: bob + 4
-    });
-  },
-  function add(bob, next) {
-    next({
-      bob: bob + 4
-    });
-  }
+  add,
+  add
 ]).run(function done(glom) {
   assert.equal(glom.bob, 13);
-});
+}, error);
 
 // Simple failure
 new Glom({
   bob: 5
 }, [
-  function fail(bob, next, error) {
-    if (bob === 1) {
-      next();
-    } else {
-      error('This failed');
-    }
-  }
-]).run(function done(glom) {
-  assert.equal(glom.bob, 13);
+  failCheck
+]).run(function done() {
+  assert.fail('This should not have been called');
+}, function error(messages) {
+  assert.equal(messages.length, 1);
+  assert.equal(messages[0], 'This failed');
+});
+
+// Multi-step failure
+new Glom({
+  bob: 5
+}, [
+  add,
+  fail
+]).run(function done() {
+  assert.fail('This should not have been called');
+}, function error(messages) {
+  assert.equal(messages.length, 1);
+  assert.equal(messages[0], 'This failed');
+});
+
+// Conditional failure
+new Glom({
+  bob: 5
+}, [
+  add,
+  [fail, add]
+]).run(function done() {
+  assert.fail('This should not have been called');
+}, function error(messages, glom) {
+  console.log(glom);
+  assert.equal(messages.length, 1);
+  assert.equal(messages[0], 'This failed');
 });
